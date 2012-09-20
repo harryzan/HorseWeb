355e30a42caba58e7bc7a0b11a81a4c15b3abceb
Zhan Yong Lin <harryzan@gmail.com>
2012-09-20 13:20:58 +0800

    initial

---
+++ java/web/src/main/java/com/horseweb/core/domain/BaseDomain.java
@@ -0,0 +1,16 @@
+package com.horseweb.core.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: harry
+ * Date: 12-3-19
+ * Time: 下午2:47
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class BaseDomain {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+}
